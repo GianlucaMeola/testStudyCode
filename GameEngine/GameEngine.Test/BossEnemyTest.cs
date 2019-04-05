029d1961084b8f15d04f10a646331a62f5d852f8
Gianluca Meola <Gianluca.Meola@tlcmarketing.com>
2019-04-05 17:54:33 +0100

    Gianluca Code

---
+++ GameEngine/GameEngine.Test/BossEnemyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using Xunit;
+
+namespace GameEngine.Test
+{
+    public class BossEnemyTest
+    {
+        [Fact]
+        private void BossHaveTheRightPower()
+        {
+            //arrange
+            Enemy sut =new BossEnemy();
+
+            //act
+
+            //assert
+            Assert.Equal(166.67, sut.TotalSpecialAttackPower,2);
+        }
+    }
+}
