8e855c41ea1663f164895c853d911eada94c53be
Gianluca Meola <Gianluca.Meola@tlcmarketing.com>
2019-04-09 15:12:25 +0100

    last version

---
+++ GameEngine/GameEngine.Tests/NonPlayerCharacterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+namespace GameEngine.Tests
+{
+    public class NonPlayerCharacterTest
+    {
+        /*[Theory]
+        [InlineData(0,100)]
+        [InlineData(1,99)]
+        [InlineData(50,50)]
+        [InlineData(101,1)]*/
+        [Theory]
+        //[MemberData("TestData", MemberType = typeof(InternalHealthDamageTestData))]
+        //or
+        //[MemberData(nameof(ExternalHealthDamageTestData.TestData), MemberType = typeof(ExternalHealthDamageTestData))]
+        //or
+        [HealthDamageData]
+        public void TakeDamage(int damage, int expectedHealt)
+        {
+            //arrange
+            NonPlayerCharacter sut = new NonPlayerCharacter();
+            
+            //act
+            sut.TakeDamage(damage);
+
+            //assert
+            Assert.Equal(expectedHealt, sut.Health);
+        }
+    }
+}
