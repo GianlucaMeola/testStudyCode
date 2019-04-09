33376b30b65013ce82f148cdfe113098cf757a00
Gianluca Meola <Gianluca.Meola@tlcmarketing.com>
2019-04-09 17:41:06 +0100

    Starting Moq

---
+++ CreditCard_Moq/CreditCardApplications.Tests/CreditCardApplicationEvaluatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace CreditCardApplications.Tests
+{
+    public class CreditCardApplicationEvaluatorTest
+    {
+        [Fact]
+        public void AcceptHightIncomeApplications()
+        {
+            //arrange
+            var sut = new CreditCardApplicationEvaluator(null);
+
+            var application = new CreditCardApplication {GrossAnnualIncome = 100_000};
+
+            //act
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            //assert
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted, decision);
+        }
+
+        [Fact]
+        public void ReferYoungApplications()
+        {
+            //arrange
+            var sut = new CreditCardApplicationEvaluator(null);
+            var application = new CreditCardApplication {Age = 19};
+
+            //act
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            //assert
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+    }
+}
